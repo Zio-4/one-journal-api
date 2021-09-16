22d708035b2a99c97793c24914a1e264093358a1
Philip Ziolkowski <philipz4848@gmail.com>
2021-09-15 17:55:16 -0700

    adding validations for journal model

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,4 +1,7 @@
 class Journal < ApplicationRecord
     belongs_to :user
     has_many :journal_posts
+
+    validates :title, presence: true, length: {maximum: 40}
+    validates :description, allow_blank: true
 end
